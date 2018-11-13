010be2bcbc9e3bfaae28a0b1d85b2619dbedc95c
Lorenzo <lcannafa@eafit.edu.co>
2018-11-13 18:03:07 -0500

    QUitando el viejo

--- doc_to_pdf.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import shutil
-
-input_filename = 'Reporte 0.docx'
-output_filename = 'output.pdf'
-
-p = subprocess.Popen(['unoconv', '--stdout', input_filename], stdout=subprocess.PIPE)
-with open(output_filename, 'w') as output:
-   shutil.copyfileobj(p.stdout, output)
--- quickstart.py
+++
@@ -1,9 +0,0 @@
-import dropbox
-
-def subir_archivo(reporte):
-    dbx = dropbox.Dropbox('8M2rk1A9ebAAAAAAAAAACqdQetGZhLSBSeh8mCjOo4_JOrs251ya-bRZcGlBZWXV')
-    with open(reporte, 'rb') as f:
-        dbx.files_upload(f.read(), '/Reportes/' + reporte)
-        link = dbx.files_get_temporary_link('/Reportes/' + reporte)
-
-    return link
